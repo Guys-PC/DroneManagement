3754a3f4f27ee487d0097054747307ab0e5c907e
Nasa1423 <ars.suhman@yandex.ru>
2020-08-13 14:40:26 +0300

    Added visualization crossing test (commented)

--- flightController.py
+++ flightController.py
@@ -19,22 +19,24 @@ def getCoef(x1, y1, x2, y2):
     
 
 def checkForIntersect(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2):
-    crossX, crossY = 0, 0
+    isIntersecting = 1
     xCross, yCross = 0, 0
     k1, b1 = getCoef(ax1, ay1, ax2, ay2)
     k2, b2 = getCoef(bx1, by1, bx2, by2)
     if k1 == k2:
         if b1 == b2:
-            print("2")
+            isIntersecting = 2
         else:
-            print("1")
+            isIntersecting = 1
     else:
+        isIntersecting = 0
         if b1 == b2:
-            crossX, crossY = 0, b1
+            xCross, yCross = 0, b1
         else:
             xCross = (b2 - b1) / (k1 - k2)
             yCross = k1 * xCross + b1
-
+    return isIntersecting, xCross, yCross
+    
 #[[xStart, yStart], [xEnd, yEnd], startTime, length]
 
 def getCoords(inp):
@@ -51,7 +53,6 @@ routes = []
 sortedRoutes = []
 
 #state: startCoords, endCoords, timeOfStart, length
-
 for i in range(5):
     routes.append([])
 
@@ -76,19 +77,42 @@ for i in range(50):
         for i in range(5):
             anyCrossing = False
             for r in routes[i]:
-               isCrossing = checkForIntersect(x1New, y1New, x2New, y2New, r[0][0], r[0][1], r[1][0], r[1][1])
-               if isCrossing == True:
+               isCrossing, x, y = checkForIntersect(x1New, y1New, x2New, y2New, r[0][0], r[0][1], r[1][0], r[1][1])
+               if isCrossing == 0:
                    anyCrossing = True
                    print("failed")
                    break
-            if anyCrossing == False:
+            if anyCrossing == 1 or anyCrossing == 2:
                 wasPassed = True
                 print("passed")
                 chosenLevel = i
                 break
     routes[chosenLevel].append([[x1New, y1New], [x2New, y2New], time.time(), sqrt(sqr(x2New - x1New) + sqr(y2New - y1New))])
     print("Added new route on " + str(chosenLevel))
+    
+    """fig = plt.figure()
+    
+    ax1New = random.randint(0, 100)
+    ay1New = random.randint(0, 100)
 
+    ax2New = random.randint(0, 100)
+    ay2New = random.randint(0, 100)
+    
+    bx1New = random.randint(0, 100)
+    by1New = random.randint(0, 100)
 
-                                                          
-        
+    bx2New = random.randint(0, 100)
+    by2New = random.randint(0, 100)
+    
+    isCrossing, x, y = checkForIntersect(ax1New, ay1New, ax2New, ay2New, bx1New, by1New, bx2New, by2New)
+    
+    graph1 = plt.plot([ax1New, ay1New], [ax2New, ay2New])
+    graph2 = plt.plot([bx1New, by1New], [bx2New, by2New])
+    
+    if isCrossing == 0:
+        print("true")
+    else:
+        print("false")
+    
+    plt.show()"""
+    
